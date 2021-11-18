8ca3dfb5d787f99d16c9dfde9fe71e3f4371d53d
mpillet <82315463+mpillet@users.noreply.github.com>
2021-11-18 13:10:37 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ def printing():
     print('coucou')
 
 
-def move(): # start counting
+def move():
     global motor_left_target, motor_right_target
     print('test boucle')
     motor_left_target = 100
