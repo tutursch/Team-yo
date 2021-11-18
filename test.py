217a313b95e4aebf95f1bf43d6920ad1712615ee
mpillet <82315463+mpillet@users.noreply.github.com>
2021-11-18 13:06:23 +0100

    test onevent

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 def printing():
     print('coucou')
+
+
+def move(): # start counting
+    global motor_left_target, motor_right_target
+    print('test boucle')
+    motor_left_target = 100
+    motor_right_target = -100
