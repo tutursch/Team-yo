bc0e04979ddcf954599de81e21e56dc8b1c56a13
BrendanBroccard <82571630+BrendanBroccard@users.noreply.github.com>
2021-12-01 15:34:47 +0100

    Création du contrôleur + local nav en général
    Il reste à scale les valeurs en fonctions des input qu'on recevra de Kalman

--- local_nav.py
+++ local_nav.py
@@ -23,32 +23,71 @@ def sensor_val_to_cm_dist(val):
     return np.asscalar(f(val))
 
 def checkObstacle(sensor):
+    print("checkObstacle")    
 
-    obstacleTrigger = 50
+    obstacle_trigger = 10
     obstacle = False
 
     for i in range(len(sensor)-2) :
-        if sensor[i] > obstacleTrigger :
+        if sensor[i] > obstacle_trigger :
             obstacle = True
 
+    print("obstacle = ")
+    print(obstacle)
     return obstacle
 
-def avoidObstacle(sensor, motorsInit):
-    w_l = [40,  20, -20, -20, -40,  30, -10]
-    w_r = [-40, -20, -20,  20,  40, -10,  30]
+def avoidObstacle(sensor):
+    print("avoidObstacle")
+    w = [40,  20, -20, -20, -40]
 
     # Scale factors for sensors and constant factor
-    sensor_scale = 1000
-    constant_scale = 20
-    
-    motorsOut = motorsInit
+    sensor_scale = 800
+    avoidance_speed = 0
 
     for i in range(len(sensor)-2) :
         # Get and scale inputs
         sensor[i] = sensor[i] // sensor_scale
             
         # Compute outputs of neurons and set motor powers
-        motorsOut[0] = motorsOut[0] + sensor[i] * w_r[i]
-        motorsOut[1] = motorsOut[1] + sensor[i] * w_l[i]
-    
-    return motorsOut
+        avoidance_speed += sensor[i] * w[i]
+
+    print("avoidance_speed = ")
+    print(avoidance_speed)
+    return avoidance_speed
+
+def pdController(orientation, old_orientation, goal_orientation) :
+    print("pdController")
+    kp = 10
+    kd = 1
+
+    error = goal_orientation-orientation
+    old_error = goal_orientation-old_orientation
+
+    control_speed = kp * error + kd * (error-old_error)
+
+    print("control_speed = ")
+    print(control_speed)
+    return control_speed
+
+
+def localNavigation(theta, old_theta, goal_theta, sensors) :
+    print("localNavigation")
+    basic_speed = 100
+
+    control_speed = 0
+    control_speed = pdController(theta, old_theta, goal_theta)
+
+    avoidance_speed = 0
+    obstacle = False
+    obstacle = checkObstacle(sensors)
+    if obstacle : 
+        avoidance_speed = avoidObstacle(sensors)
+
+    left_speed = basic_speed - control_speed + avoidance_speed
+    right_speed = basic_speed + control_speed - avoidance_speed
+
+    print("left_speed = ")
+    print(left_speed)
+    print("right_speed = ")
+    print(right_speed)
+    return left_speed, right_speed
