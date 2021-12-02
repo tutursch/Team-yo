755ceefc3a4f35d956984606d1384305db7bbdde
fiferrei <82997688+fiferrei@users.noreply.github.com>
2021-12-02 20:49:20 +0100

    all together

--- local_nav.py
+++ local_nav.py
@@ -41,12 +41,11 @@ def checkObstacle(sensor):
         if sensor[i] > obstacle_trigger :
             obstacle = True
 
-    print("obstacle = ")
-    print(obstacle)
+
     return obstacle
 
 def avoidObstacle(sensor):
-    print("avoidObstacle")
+
     w = [40,  20, -20, -20, -40]
 
     # Scale factors for sensors and constant factor
@@ -60,43 +59,38 @@ def avoidObstacle(sensor):
         # Compute outputs of neurons and set motor powers
         avoidance_speed += sensor[i] * w[i]
 
-    print("avoidance_speed = ")
-    print(avoidance_speed)
+
     return avoidance_speed
 
 def pdController(orientation, old_orientation, goal_orientation) :
-    print("pdController")
+
     kp = 10
-    kd = 1
+    kd = 5
 
     error = goal_orientation-orientation
     old_error = goal_orientation-old_orientation
+    print("error = ")
+    print(error)
 
     control_speed = kp * error + kd * (error-old_error)
 
-    print("control_speed = ")
-    print(control_speed)
+
     return control_speed
 
 
 def localNavigation(theta, old_theta, goal_theta, sensors) :
-    print("localNavigation")
+
     basic_speed = 100
 
-    control_speed = 0
-    control_speed = pdController(theta, old_theta, goal_theta)
+    control_speed = 3*pdController(theta, old_theta, goal_theta)
 
     avoidance_speed = 0
-    obstacle = False
+
     obstacle = checkObstacle(sensors)
     if obstacle : 
         avoidance_speed = avoidObstacle(sensors)
 
-    left_speed = basic_speed - control_speed + avoidance_speed
-    right_speed = basic_speed + control_speed - avoidance_speed
+    left_speed = basic_speed + control_speed + avoidance_speed
+    right_speed = basic_speed - control_speed - avoidance_speed
 
-    print("left_speed = ")
-    print(left_speed)
-    print("right_speed = ")
-    print(right_speed)
     return left_speed, right_speed
--- update_pos.py
+++ update_pos.py
@@ -1,50 +1,52 @@
 import numpy as np
 import cv2
 from numpy.lib.twodim_base import mask_indices
-import time
 
 VideoCap = cv2.VideoCapture(0)
 
 def detect_thymio():
     top=[]
     bottom=[]
-    ret, frame = VideoCap.read()
-    color_top = 130
-    color_bottom = 20
-    lo_top = np.array([color_top-5, 100, 50])
-    hi_top = np.array([color_top+5, 255,255])
-    lo_bottom = np.array([color_bottom-5, 100, 50])
-    hi_bottom = np.array([color_bottom+5, 255,255])
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    image = cv2.blur(image, (5, 5))
-    mask_top = cv2.inRange(image, lo_top, hi_top)
-    mask_bottom = cv2.inRange(image, lo_bottom, hi_bottom)
-    mask_top = cv2.erode(mask_top, None, iterations=2)
-    mask_top = cv2.dilate(mask_top, None, iterations=2)
-    mask_bottom = cv2.erode(mask_bottom, None, iterations=2)
-    mask_bottom = cv2.dilate(mask_bottom, None, iterations=2)
-    elements_top = cv2.findContours(mask_top, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    elements_top = sorted(elements_top, key=lambda x:cv2.contourArea(x), reverse=True)
-    elements_bottom = cv2.findContours(mask_bottom, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    elements_bottom = sorted(elements_bottom, key=lambda x:cv2.contourArea(x), reverse=True)
-    if len(elements_top) > 0:
-        c=max(elements_top, key=cv2.contourArea)
-        ((x, y), radius)=cv2.minEnclosingCircle(c)
-        top.append(np.array([int(x), int(y)]))
-    if len(elements_bottom) > 0:
-        c=max(elements_bottom, key=cv2.contourArea)
-        ((x, y), radius)=cv2.minEnclosingCircle(c)
-        bottom.append(np.array([int(x), int(y)]))
-    if (len(bottom)>0 and len(top)>0):
-        delta_x = top[0][0] - bottom[0][0]
-        delta_y = top[0][1] - bottom[0][1]
+ 
+    while(len(bottom) == 0 or len(top) == 0):
+        ret, frame = VideoCap.read()
+        color_top = 135
+        color_bottom = 17
+        lo_top = np.array([color_top-20, 80, 40])
+        hi_top = np.array([color_top+20, 150,150])
+        lo_bottom = np.array([color_bottom-20, 95, 110])
+        hi_bottom = np.array([color_bottom+20, 115,200])
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        image = cv2.blur(image, (5, 5))
+        mask_top = cv2.inRange(image, lo_top, hi_top)
+        mask_bottom = cv2.inRange(image, lo_bottom, hi_bottom)
+        mask_top = cv2.erode(mask_top, None, iterations=2)
+        mask_top = cv2.dilate(mask_top, None, iterations=2)
+        mask_bottom = cv2.erode(mask_bottom, None, iterations=2)
+        mask_bottom = cv2.dilate(mask_bottom, None, iterations=2)
+        elements_top = cv2.findContours(mask_top, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        elements_top = sorted(elements_top, key=lambda x:cv2.contourArea(x), reverse=True)
+        elements_bottom = cv2.findContours(mask_bottom, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        elements_bottom = sorted(elements_bottom, key=lambda x:cv2.contourArea(x), reverse=True)
+        if len(elements_top) > 0:
+            c=max(elements_top, key=cv2.contourArea)
+            ((x, y), radius)=cv2.minEnclosingCircle(c)
+            if(len(top)==0):
+                top.append(np.array([int(x), int(y)]))
+        if len(elements_bottom) > 0:
+            c=max(elements_bottom, key=cv2.contourArea)
+            ((x, y), radius)=cv2.minEnclosingCircle(c)
+            if(len(bottom)==0):
+                bottom.append(np.array([int(x), int(y)]))
+        if (len(bottom)>0 and len(top)>0):
+            delta_x = top[0][0] - bottom[0][0]
+            delta_y = top[0][1] - bottom[0][1]
 
-        if (delta_x !=0):
-            angle = np.arctan2(delta_y, delta_x)
-        else:
-            angle = np.pi/2
+            if (delta_x !=0):
+                angle = np.arctan2(delta_y, delta_x)
+            else:
+                angle = np.pi/2
             if(angle < 0):
                 angle = angle + 2*np.pi
-        return bottom, angle
-    else :
-        return 0
+           
+            return bottom[0], angle
